65d58b885c0a66094a26fae9bc6590c8ce17dd9f
pod524 <73357152+pod524@users.noreply.github.com>
2023-12-12 23:43:50 +0000

    Delete palindrome-inspector directory

--- palindrome-inspector/src/main/java/palindromeinspector/repository/PalindromeRepository.java
+++
@@ -1,9 +0,0 @@
-package palindromeinspector.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import palindromeinspector.model.PalindromeRequest;
-
-@Repository
-public interface PalindromeRepository extends JpaRepository<PalindromeRequest, Long> {
-}
